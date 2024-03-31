3ad36c642d0fca2b8679c284410efbab90bf6ffd
Nadezhda26m <nadezhda-26m@mail.ru>
2024-03-31 13:43:23 +0300

    fix Springdoc OpenAPI

--- outlet/src/main/java/com/kirin/outlet/OutletServiceApplication.java
+++ outlet/src/main/java/com/kirin/outlet/OutletServiceApplication.java
@@ -2,12 +2,16 @@ package com.kirin.outlet;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Outlet Service", version = "1.0",
-        description = "Documentation Outlet Service v1.0"))
+        description = "Documentation Outlet Service v1.0"),
+        servers = {
+                @Server(url = "/outlet", description = "Сервис для работы с заказами на предприятии")
+        })
 public class OutletServiceApplication {
 
     public static void main(String[] args) {
