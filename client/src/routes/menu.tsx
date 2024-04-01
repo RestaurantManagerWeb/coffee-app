0e43da937bdc1fe55fdb754a096c1417871e2fa5
noisyboy25 <dj402and@gmail.com>
2024-04-01 20:35:28 +0300

    Add fetch from server

--- client/src/routes/menu.tsx
+++ client/src/routes/menu.tsx
@@ -1,10 +1,28 @@
 import { Table, TableData } from '@mantine/core';
-import { menuItems } from '../test-data';
+import { useEffect, useState } from 'react';
+import { MenuItem } from '../types';
 
 function Menu() {
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  useEffect(() => {
+    (async () => {
+      const groupId = 1;
+      let data: MenuItem[] = [];
+      try {
+        const res = await fetch(`/api/outlet/menu/group?id=${groupId}`);
+        data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+      setMenuItems(data);
+    })();
+  }, []);
+
   const tableData: TableData = {
     head: ['ID', 'Name', 'Price'],
-    body: menuItems.map((item) => Object.values(item)),
+    body: menuItems.map((item) => [item.id, item.name, item.price]),
   };
 
   return (
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,5 +1,15 @@
+export type MenuGroup = {
+  id: number;
+  name: string;
+};
+
 export type MenuItem = {
   id: number;
   name: string;
   price: number;
+  vat: number;
+  inStopList: boolean;
+  menuGroupId: number;
+  stockItemId: number;
+  processChartId: number;
 };
